56d294a988e2113ad1ef7da1ad042b7cb918c733
LeonardoNakashima <157625776+LeonardoNakashima@users.noreply.github.com>
2025-04-25 15:10:08 -0300

    modificando a tela notas

--- projeto-angular/src/app/app.routes.ts
+++ projeto-angular/src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { HeaderComponent } from './layout/header/header.component';
 import { LoginComponent } from './auth/login/login.component';
 import { CadastroComponent } from './auth/cadastro/cadastro.component';
 import { GraficoComponent } from './auth/grafico/grafico.component';
+import { NotasComponent } from './auth/notas/notas.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent},
     { path: '', component: LoginComponent},
     { path: 'cadastro', component: CadastroComponent},
-    { path: 'grafico', component: GraficoComponent}
+    { path: 'grafico', component: GraficoComponent},
+    { path: 'notas', component: NotasComponent}
     
 ];
--- projeto-angular/src/app/auth/notas/notas.component.ts
+++ projeto-angular/src/app/auth/notas/notas.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../layout/header/header.component';
+import { SidebarComponent } from '../../layout/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-notas',
-  imports: [],
+  imports: [HeaderComponent,SidebarComponent],
   templateUrl: './notas.component.html',
   styleUrl: './notas.component.css'
 })
